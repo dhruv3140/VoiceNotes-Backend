a5e5163cfd5be812313f25006877b609788c469e
Sarvagya Singhal <dhruvsinghal166@gmail.com>
2026-08-17 18:25:37 +0530

    origins

--- main.py
+++ main.py
@@ -17,9 +17,15 @@ from utils.sms_service import send_sms_via_gateway
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://voice-notes-frontend-lemon.vercel.app",  
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://voice-notes-frontend-lemon.vercel.app", "http://localhost:5173"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
